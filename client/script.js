3b06925aa331632a6407b079ca77eaa092efe015
Himanshu yadav <002himmuy@gmail.com>
2023-04-27 21:00:39 +0530

    deployment changes

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // sending prompt back to the server and getting the chatGpt bot's response
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://chatgpt-api-x8cq.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
